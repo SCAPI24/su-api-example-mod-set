52f7bf0e5ad452b2d07171d772b91844b5b0b086
SC-SPM <sc-spm@gitee.com>
2026-06-12 18:41:51 +0800

    feat: 添加PakExplorer工具 + CircuitAutoRouter更新
    PakExplorer: Survivalcraft Content.pak文件浏览器
    - Avalonia UI界面，左侧文件夹树+右侧文件列表
    - 支持XOR 0x3F解密TOC和内容区
    - 根据TypeName反序列化为标准格式提取(XML/PNG/WAV等)
    - RawRgba纹理数据转PNG(ImageSharp)
    - 子文件夹显示和双击导航
    - 右键提取/导入替换/撤销修改
    - 文件类型emoji图标
    
    CircuitAutoRouter: 小更新
    - 删除备份文件SubsystemCircuitRouter.cs.bak

---
+++ PakExplorer/ViewModels/ViewModelBase.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PakExplorer.ViewModels;
+
+public class ViewModelBase : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+    {
+        if (Equals(field, value))
+        {
+            return false;
+        }
+        field = value;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        return true;
+    }
+
+    protected void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
