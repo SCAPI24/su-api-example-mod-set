99baf51e4ab070417b7be11d62d90699d4fd7757
SC-SPM <sc-spm@gitee.com>
2026-07-19 22:12:18 +0800

    feat: optimize multiplayer sync and joining

--- ScMultiplayer/Message/GamePakWorldReadyMessage.cs
+++ ScMultiplayer/Message/GamePakWorldReadyMessage.cs
@@ -7,24 +7,28 @@ namespace ScMultiplayer
     public class GamePakWorldReadyMessage : Message
     {
         public int TransferId;
+        public bool IsProjectReady;
 
         public GamePakWorldReadyMessage()
         {
         }
 
-        public GamePakWorldReadyMessage(int transferId)
+        public GamePakWorldReadyMessage(int transferId, bool isProjectReady = true)
         {
             TransferId = transferId;
+            IsProjectReady = isProjectReady;
         }
 
         protected override void Read(SuReader reader)
         {
             TransferId = reader.ReadInt32();
+            IsProjectReady = reader.ReadBoolean();
         }
 
         protected override void Write(SuWriter writer)
         {
             writer.WriteInt32(TransferId);
+            writer.WriteBoolean(IsProjectReady);
         }
     }
 }
