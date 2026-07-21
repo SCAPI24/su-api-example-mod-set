9689a0eea8a4bd9e86e44999a5d9ac9ae0f66a46
SC-SPM <sc-spm@gitee.com>
2026-07-21 14:32:12 +0800

    fix: use reliable UDP for world transfers

--- ScMultiplayer/Message/GamePakWorldRepairRequestMessage.cs
+++ ScMultiplayer/Message/GamePakWorldRepairRequestMessage.cs
@@ -9,7 +9,6 @@ namespace ScMultiplayer
     {
         public int TransferId;
         public bool RequestManifest;
-        public bool RequestUdpFallback;
         public int HighestContiguousChunkIndex = -1;
         public int HighestReceivedChunkIndex = -1;
         public int[] MissingChunkIndices = Array.Empty<int>();
@@ -18,7 +17,6 @@ namespace ScMultiplayer
         {
             TransferId = reader.ReadInt32();
             RequestManifest = reader.ReadBoolean();
-            RequestUdpFallback = reader.ReadBoolean();
             HighestContiguousChunkIndex = reader.ReadInt32();
             HighestReceivedChunkIndex = reader.ReadInt32();
             int count = reader.ReadPackedInt32();
@@ -33,7 +31,6 @@ namespace ScMultiplayer
         {
             writer.WriteInt32(TransferId);
             writer.WriteBoolean(RequestManifest);
-            writer.WriteBoolean(RequestUdpFallback);
             writer.WriteInt32(HighestContiguousChunkIndex);
             writer.WriteInt32(HighestReceivedChunkIndex);
             writer.WritePackedInt32(MissingChunkIndices?.Length ?? 0);
